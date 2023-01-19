6686518749ab9ca5f0c5e39969443a1034eddd12
unknown <nikhilpardeshi58@gmail.com>
2023-01-19 15:44:12 +0530

    picker drop under maintenece

--- Cal.js
+++ Cal.js
@@ -8,9 +8,26 @@ const Cal = ({val}) => {
     const [open, setOpen] = useState(false);
   const [value1, setValue] = useState(null);
   const [items, setItems] = useState([
-    {label: 'Apple', value: 'apple'},
-    {label: 'Banana', value: 'banana'}
+    {label: 'January', value: 'January'},
+    {label: 'Febuary', value: 'Febuary'},
+    {label: 'March', value: 'March'},
+    {label: 'April', value: 'April'},
+    {label: 'May', value: 'May'},
+    {label: 'June', value: 'June'},
+    {label: 'July', value: 'July'},
+    {label: 'August', value: 'August'},
+    {label: 'September', value: 'September'},
+    {label: 'October', value: 'October'},
+    {label: 'November', value: 'November'},
+    {label: 'December', value: 'December/'},
   ]);
+  const [open1, setOpen1] = useState(false);
+  const [value11, setValue1] = useState(null);
+  const [items1, setItems1] = useState([
+    {label: '2022', value: '2022'},
+{label: '2023', value: '2023'}
+  ]);
+
    
     
   return (
@@ -26,18 +43,32 @@ const Cal = ({val}) => {
       >
         <View style={styles.centeredView}>
         
-        <Text style={styles.paragraph}>
-        Select Record Month
+        <Text style={{textAlign:"center"}}>
+        Select Year & Month
       </Text>
-      <DropDownPicker
+      <View style={{flexDirection:"row",paddingHorizontal:10,width:"80%",alignItems:"center",justifyContent:"center",margin:5}}>
+      <DropDownPicker style={{marginLeft:30}}
       open={open}
       value={value1}
       items={items}
+      containerStyle={{width: 150}}
       setOpen={setOpen}
       setValue={setValue}
       setItems={setItems}
       onChangeValue={(rec) => console.log(rec)}
     />
+ 
+  <DropDownPicker style={{marginLeft:35}}
+      open={open1}
+      value={value11}
+      items={items1}
+      containerStyle={{width: 150}}
+      setOpen={setOpen1}
+      setValue={setValue1}
+      setItems={setItems1}
+      onChangeValue={(rec) => console.log(rec)}
+    />
+    </View>
 
         
         </View>
@@ -55,7 +86,7 @@ const styles = StyleSheet.create({
     centeredView: {
       flex: 1,
       justifyContent: "center",
-      alignItems: "center",
+     
       marginTop: 22
     },
     modalView: {
@@ -99,3 +130,8 @@ const styles = StyleSheet.create({
   });
 
 export default Cal;
+
+
+
+
+
